e041f367a30bff38a8a6059f38b3356b5a83143b
jandzouana <jandzouana@alcacruz.com>
2024-07-17 12:35:53 -0700

    started work on create post

--- src/constants/interfaces.ts
+++ src/constants/interfaces.ts
@@ -1,11 +1,25 @@
+import React from "react";
+
 export interface CardProps {
 
 }
 
 export interface FormFieldProps {
-
+    labelName: string;
+    type: string;
+    name: string;
+    placeholder: string;
+    value: string;
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    isSupriseMe?: boolean;
 }
 
 export interface LoaderProps  {
 
+}
+
+export interface PostForm{
+    name: string;
+    prompt: string;
+    photo: string;
 }
